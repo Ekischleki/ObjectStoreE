48478299b2daa0d7e53f1681024e5eee81aac90f
Erik Wolfschläger <e.wolfschlaeger@gmail.com>
2024-01-05 18:54:50 +0100

    Fixed formatter bug

--- Formatter.cs
+++ Formatter.cs
@@ -13,26 +13,27 @@ namespace ObjectStoreE
         {
             sb.Append('\t', (int)amount);
         }
-        private static void Format(Region region, StringBuilder sb, uint tabamount) 
+        private static void Format(Region region, StringBuilder sb, uint tabamount, bool escapeValues) 
         {
             AppendTabamount(sb, tabamount);
             sb.Append('§').Append(region.regionName).AppendLine(";");
             foreach(var directValue in region.DirectValues)
             {
                 AppendTabamount(sb, tabamount + 1);
-                sb.Append(directValue.name).Append(':').Append(DirectValueClearify.EncodeInvalidChars(directValue.value)).AppendLine(";");
+                
+                sb.Append('-').Append(directValue.name).Append(':').Append(escapeValues ? DirectValueClearify.EncodeInvalidChars(directValue.value) : (directValue.value ?? "<null>")).AppendLine(";");
             }
             foreach(var subRegion in region.SubRegions)
             {
-                Format(subRegion, sb, tabamount + 1);
+                Format(subRegion, sb, tabamount + 1, escapeValues);
             }
             AppendTabamount(sb, tabamount);
             sb.AppendLine("$;");
         }
-        public static string FormatRegion(Region region)
+        public static string FormatRegion(Region region, bool escapeValues = true)
         {
             StringBuilder sb = new StringBuilder();
-            Format(region, sb, 0);
+            Format(region, sb, 0, escapeValues);
             return sb.ToString();
         }
 
